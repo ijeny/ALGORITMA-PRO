4779cd8bc15f1e1714159a189825123f46ed1228
ijeny <imtinan.jmb6628@gmail.com>
2024-10-21 20:19:10 +0700

    Add files via upload

---
+++ math_modul.py
@@ -0,0 +1,12 @@
+import math
+
+akar = math.sqrt(25)
+pangkat = math.pow(4,3)
+bulat_bawah = math.floor(1.6)
+bulat_atas = math.ceil(1.6)
+print()
+print(f"akar 25 = {akar}")
+print(f"4 pangkat 3 = {pangkat}")
+print(f"pembulatan ke bawah dari 1.6 = {bulat_bawah}")
+print(f"pembulatan ke atas dari 1.6 = {bulat_atas}")
+print()
